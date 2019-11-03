855711c30c9c5129f1792679ee11a783b86993fd
Jeremy Lynch <jeremylynch1994@gmail.com>
2019-11-03 16:31:57 +1000

    Uses JSON navigation

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,7 @@ import {
   DropdownMenu,
   DropdownItem } from 'reactstrap';
 import Link from 'gatsby-link'
+import JsonNav from './jsonNav'
 
 const Header = (props) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,41 +19,13 @@ const Header = (props) => {
   const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <div>
-      <Navbar color="light" light expand="md">
-        <Link to="/" className="navbar-brand">{props.siteTitle}</Link>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <Link to="/page-2" className="nav-link">
-                Page 2
-              </Link>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/jeremylynch/gatsby-strapi-starter">GitHub</NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Option 1
-                </DropdownItem>
-                <DropdownItem>
-                  Option 2
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>
-                  Reset
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-        </Collapse>
-      </Navbar>
-    </div>
+    <Navbar color="light" light expand="md">
+      <Link to="/" className="navbar-brand">{props.siteTitle}</Link>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <JsonNav/>
+      </Collapse>
+    </Navbar>
   );
 }
 
