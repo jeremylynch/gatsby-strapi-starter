f5448d819a04d4d04ee50e185f2caa1a9cca8059
Jeremy Lynch <jeremylynch1994@gmail.com>
2019-11-04 17:29:59 +1000

    fix siteURL

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Bootstrap Strapi Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    siteUrl: `https://gatsby-strapi-starter.netlify.com/`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
