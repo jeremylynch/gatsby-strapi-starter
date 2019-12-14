b8b73c2021315c3f611203f85730e47a7d6ff85c
Jeremy Lynch <jeremylynch1994@gmail.com>
2019-12-14 11:04:31 +1000

    Update slider.js

--- src/components/slider.js
+++ src/components/slider.js
@@ -1,38 +1,44 @@
 import React from "react";
 import Slider from "react-slick";
+import { useStaticQuery, graphql } from "gatsby"
 
-class SimpleSlider extends React.Component {
-  render() {
-    var settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    };
-    return (
-      <Slider {...settings}>
-        <div>
-          <div className="text-center">
-            <h1>Bootstrap + Strapi + Gatsby Starter</h1>
-            <p>By Jeremy Lynch</p>
-          </div>
+export default () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+  const title = useStaticQuery(graphql`
+    query TitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+  return (
+    <Slider {...settings}>
+      <div>
+        <div className="text-center">
+          <h1>{title.site.siteMetadata.title}</h1>
+          <p>By Jeremy Lynch</p>
         </div>
-        <div>
-          <div className="text-center">
-            <h1>Bootstrap + Strapi + Gatsby Starter</h1>
-            <p>By Jeremy Lynch</p>
-          </div>
+      </div>
+      <div>
+        <div className="text-center">
+          <h1>{title.site.siteMetadata.title}</h1>
+          <p>By Jeremy Lynch</p>
         </div>
-        <div>
-          <div className="text-center">
-            <h1>Bootstrap + Strapi + Gatsby Starter</h1>
-            <p>By Jeremy Lynch</p>
-          </div>
+      </div>
+      <div>
+        <div className="text-center">
+          <h1>{title.site.siteMetadata.title}</h1>
+          <p>By Jeremy Lynch</p>
         </div>
-      </Slider>
-    );
-  }
+      </div>
+    </Slider>
+  )
 }
-
-export default SimpleSlider
