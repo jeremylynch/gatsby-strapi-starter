9c62951a62e518f43564a1d6be951ca067e5c73b
Jeremy Lynch <jeremylynch1994@gmail.com>
2019-12-16 17:15:59 +1000

    move to styled components

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,26 @@ import React from 'react'
 import {Container, Row, Col} from 'reactstrap'
 import { FaFacebookSquare, FaInstagram } from 'react-icons/fa';
 import Link from './link';
+import styled from "styled-components"
+
+const Footer = styled.footer`
+  padding: 3rem 0;
+  background: #f8f9fa;
+  a, a:hover {
+    color: inherit;
+  }
+  ul {
+    color: rgba(0, 0, 0, 0.5);
+    -webkit-padding-start: 0;
+    padding: 0;
+    & > li {
+      list-style: none;
+      a, a:hover {
+        color: inherit;
+      }
+    }
+  }
+`
 
 let SocialLink = ({Icon}) => (
   <Link to="/" className="mr-2">
@@ -18,7 +38,7 @@ let FooterLink = ({to, children}) => (
 )
 
 export default () => (
-  <footer className="py-5 bg-light">
+  <Footer>
     <Container>
       <Row>
         <Col>
@@ -46,5 +66,5 @@ export default () => (
         </Col>
       </Row>
     </Container>
-  </footer>
+  </Footer>
 )
