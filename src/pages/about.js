3ba162267227170978fca66a8c556207a9fcdfe9
Jeremy Lynch <jeremylynch1994@gmail.com>
2021-01-03 16:43:02 +1000

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import PageTemplate from '../components/PageTemplate'
+import PageTemplate from '../components/pageTemplate'
 
 export default () => (
   <PageTemplate title="About Us">
